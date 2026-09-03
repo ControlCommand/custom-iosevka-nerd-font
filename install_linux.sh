b41e5a909da524dcba375708926412d5f7041c57
ControlCommand <63528008+ControlCommand@users.noreply.github.com>
2026-09-02 20:55:48 -0700

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sudo mkdir -p /usr/share/fonts/truetype/afio
-sudo cp -f -v _output/*.ttf /usr/share/fonts/truetype/afio
+sudo mkdir -p /usr/share/fonts/truetype/iosevkasclinic
+sudo cp -f -v _output/*.ttf /usr/share/fonts/truetype/iosevkasclinic
